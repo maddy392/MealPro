cfa685395e22bd504d64332c0870564389c369cc
maddy392 <maddy392@gmail.com>
2024-10-11 11:18:17 -0500

    Initial Commit

---
+++ MealPro/MealProApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealProApp.swift
+//  MealPro
+//
+//  Created by Madhu Babu Adiki on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MealProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
