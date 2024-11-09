4e951abb7b094f6daae4206d1a928b8d4994e3a8
maddy392 <maddy392@gmail.com>
2024-11-08 22:58:20 -0600

    added basic get recipes action group

---
+++ agent/ActionGroupsAndInvokeAgent/actionGroups/getRecipes/app.py
@@ -0,0 +1,61 @@
+import json
+import boto3
+import requests
+
+
+def lambda_handler(event, context):
+    ssm = boto3.client('ssm')
+    parameter_name = '/amplify/mealpro/madpro-sandbox-6e21c0feec/SPOONACULAR_RAPIDAPI_KEY'
+    
+    response = ssm.get_parameter(Name=parameter_name, WithDecryption=True)
+    api_key = response['Parameter']['Value']
+    # print(api_key)
+
+    agent = event['agent']
+    actionGroup = event['actionGroup']
+    function = event['function']
+    parameters = event.get('parameters', [])
+    
+    # Use the api_key in your function logic
+    url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch"
+
+    params = {item["name"]:item["value"] for item in parameters}
+    headers = {
+    'X-RapidAPI-Key': api_key
+    }
+
+    response = requests.request("GET", url, headers=headers, params=params)
+
+    recipe_results = response.json()['results']
+
+
+    return {
+    "messageVersion": "1.0",
+    "response": {
+        "actionGroup": actionGroup,
+        "function": function,
+        "functionResponse": {
+            "responseBody": {
+                "TEXT": { 
+                    "body": json.dumps(recipe_results)
+                }
+            }
+        }
+    },
+    "sessionAttributes": {
+        "string": "string",
+    },
+    "promptSessionAttributes": {
+        "string": "string"
+    },
+    "knowledgeBasesConfiguration": [
+        {
+            "knowledgeBaseId": "string",
+            "retrievalConfiguration": {
+                "vectorSearchConfiguration": {
+                    "numberOfResults": 5
+                }
+            }
+        },
+    ]
+}
